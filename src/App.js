28784fb5508d0a081373028bf466af129f78a511
TebaNady <teba7033@gmail.com>
2023-10-11 02:11:36 +0300

    Change fatal

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import './App.css';
+import Artist from './Components/Artist/Artist';
+import LayOut from './Components/LayOut/LayOut';
+import About from './Components/About/About'
+import Contact from './Components/Contact/Contact'
+import Home from './Components/Home/Home';
+const routes = createBrowserRouter([{
+  path: "", element: <LayOut />, children: [
+    { index : true, element: <Home /> },
+    { path: "home", element: <Home /> },
+    { path: "artist", element: <Artist /> },
+    { path: "about", element: <About /> },
+    { path: "contact", element: <Contact /> },
+  ]
+}])
+function App() {
+  return (
+    <RouterProvider router={routes}></RouterProvider>
+  );
+}
+
+export default App;
